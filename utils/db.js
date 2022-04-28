8d8115daf343e0a6de127be73e48d0bbf98f031b
meshtatsuo <meshtatsuo@gmail.com>
2022-04-27 19:11:04 -0600

    directory scanning works

---
+++ utils/db.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const path = require("path");
+const { Album, Artist, Track } = require("../models");
+
+// checks db for existing album
+
+// checks db for existing artist
--- utils/fileParser.js
+++ utils/fileParser.js
@@ -1,57 +1,94 @@
-// File parser 
-const mm = require('music-metadata');
-const util = require('util');
-const { getLyrics, getSong }= require('genius-lyrics-api');
-require('dotenv').config()
-const testTrack = "./testFiles/02 Let Her Go.m4a";
+// File parser
+const mm = require("music-metadata");
+const { getLyrics, getSong } = require("genius-lyrics-api");
 
-async function createTrackObject(fileDir){
-let newTrack = {};
+const Path = require("path");
+const FS = require("fs");
+const util = require("util");
+const { getFileExtension } = require("../helpers/fileExtensions");
+const { create } = require("../models/Album");
+
+let files = [];
+
+require("dotenv").config();
+
+const testDir = "G:/Music/Music";
+
+async function createTrackObject(fileDir) {
+  let newTrack = {};
   try {
-      // parse metadata from file
-    const metadata = await mm.parseFile(testTrack);
+    // parse metadata from file
+    const metadata = await mm.parseFile(fileDir);
     // if successful, create newTrack object
+    console.log(metadata);
 
     //const art = await albumArt("Breaking Benjamin", "Ember");
     const options = {
-	apiKey: process.env.GENIUS_TOKEN,
-	title: "",//metadata.common.album,
-	artist: metadata.common.artist,
-	optimizeQuery: true
-    }; 
+      apiKey: process.env.GENIUS_TOKEN,
+      title: "", //metadata.common.album,
+      artist: metadata.common.artist,
+      optimizeQuery: true,
+    };
 
     await getSong(options)
-    .then((song)=>{
-
+      .then((song) => {
         coverArt = song.albumArt;
-        
+
         newTrack = {
-        trackName: metadata.common.title,
-        trackArtist: metadata.common.artist,
-        trackAlbum: metadata.common.album,
-        trackLength: metadata.format.duration,
-        trackAlbumArt: coverArt,
-        trackPath: testTrack
-    };
-    })
-    .catch((err)=>{
+          trackName: metadata.common.title,
+          trackArtist: metadata.common.artist,
+          trackAlbum: metadata.common.album,
+          trackLength: metadata.format.duration,
+          trackAlbumArt: coverArt,
+          trackPath: fileDir,
+        };
+      })
+      .catch((err) => {
         // if fetching album art fails, use local placeholder
         newTrack = {
-        trackName: metadata.common.title,
-        trackArtist: metadata.common.artist,
-        trackAlbum: metadata.common.album,
-        trackLength: metadata.format.duration,
-        trackAlbumArt: "../public/images/placeholder_albumart.png",
-        trackPath: testTrack
-    };
-    })
+          trackName: metadata.common.title,
+          trackArtist: metadata.common.artist,
+          trackAlbum: metadata.common.album,
+          trackLength: metadata.format.duration,
+          trackAlbumArt: "../public/images/placeholder_albumart.png",
+          trackPath: fileDir,
+        };
+      });
 
     //print out to console
     console.log(newTrack);
-    
-} catch (error) {
+  } catch (error) {
     console.error(error.message);
   }
 }
 
-createTrackObject(testTrack);
+async function scanDirectory(Directory) {
+  FS.readdirSync(Directory).forEach((File) => {
+    const absolutePath = Path.join(Directory, File);
+    if (FS.statSync(absolutePath).isDirectory()) {
+      return scanDirectory(absolutePath);
+    }
+
+    if (
+      (getFileExtension(absolutePath) != "mp3") &
+      (getFileExtension(absolutePath) != "m4a") &
+      (getFileExtension(absolutePath) != "wav") &
+      (getFileExtension(absolutePath) != "aiff") &
+      (getFileExtension(absolutePath) != "ogg") &
+      (getFileExtension(absolutePath) != "flac") &
+      (getFileExtension(absolutePath) != "aac")
+    ) {
+      console.log("File not supported");
+    } else {
+      return files.push(absolutePath);
+    }
+  });
+}
+
+async function addToDataBase(dir) {
+  const track = createTrackObject(dir);
+}
+
+scanDirectory(testDir);
+console.log(files);
+files.forEach((file) => addToDataBase(file));
