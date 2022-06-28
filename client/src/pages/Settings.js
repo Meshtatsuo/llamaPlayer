b0b4ac8ae30ff680474ed3e87516eb98599ea3e5
Dominic Misasi <meshtatsuo@gmail.com>
2022-06-28 14:11:04 -0500

    add crossfade setting

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SettingsPage() {
+  const [crossfade, setCrossfade] = useState(false);
+
+  let lib = "example/lib";
+
+  const updateFade = () => {
+    console.log("before: ", crossfade);
+    //toggle crossfade update
+    if (crossfade === false) {
+      console.log("yay");
+      setCrossfade(true);
+    } else {
+      setCrossfade(false);
+    }
+    console.log(crossfade);
+  };
+
   return (
     <div className="content">
       <h1>Settings</h1>
-      <br />
-      <div id="library-settings">
+      <hr />
+      <div id="library-settings" className="settings-panel">
         <h2>Library</h2>
-        <div id="lib-select">
+        <div id="lib-display" className="settings-option">
+          <p>
+            Current Library: <span>{lib}</span>
+          </p>
+        </div>
+        <div id="lib-select" className="settings-option">
           <label for="lib-dir">Library Location: </label>
           <input type="file" />
         </div>
       </div>
+      <div id="playback-settings" className="settings-panel">
+        <div className="settings-option">
+          <h2>Playback</h2>
+          <div id="crossfade">
+            <label for="crossfade-option">Crossfade Tracks: </label>
+            <input
+              type="checkbox"
+              name="crossfade-option"
+              onChange={updateFade}
+            />
+          </div>
+        </div>
+      </div>
+      <hr />
     </div>
   );
 }
