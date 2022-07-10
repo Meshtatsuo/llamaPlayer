4cdfcf5336a2738b50d1b548020af0667eb7b1bc
Dominic Misasi <meshtatsuo@gmail.com>
2022-07-10 16:40:25 -0600

    created playlist model

---
+++ models/Playlist.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Playlist extends Model {}
+
+Playlist.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tracks: [
+      {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+          model: "track",
+          key: "id",
+        },
+      },
+    ],
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    modelName: "Playlist",
+  }
+);
